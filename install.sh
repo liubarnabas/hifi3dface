33886a82aadd2da7ff7c2d5a91303413d096b778
lb <liubarnabas@gmail.com>
2022-03-06 08:10:37 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "compiling rasterizer"
-TF_INC=/home/haoxianzhang/anaconda3/envs/py36tf18_tl/lib/python3.6/site-packages/tensorflow/include
-TF_LIB=/home/haoxianzhang/anaconda3/envs/py36tf18_tl/lib/python3.6/site-packages/tensorflow
+TF_INC=/root/anaconda3/pkgs/tensorflow-base-1.15.0-gpu_py37h9dcbed7_0/lib/python3.7/site-packages/tensorflow/include
+TF_LIB=/root/anaconda3/pkgs/tensorflow-base-1.15.0-gpu_py37h9dcbed7_0/lib/python3.7/site-packages/tensorflow
 # you might need the following to successfully compile the third-party library
 tf_mesh_renderer_path=$(pwd)/third_party/kernels/
 g++ -std=c++11 \
     -shared $tf_mesh_renderer_path/rasterize_triangles_grad.cc $tf_mesh_renderer_path/rasterize_triangles_op.cc $tf_mesh_renderer_path/rasterize_triangles_impl.cc $tf_mesh_renderer_path/rasterize_triangles_impl.h \
-    -o $tf_mesh_renderer_path/rasterize_triangles_kernel.so -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 \
+    -o $tf_mesh_renderer_path/rasterize_triangles_kernel.so -fPIC \
     -I$TF_INC -L$TF_LIB -ltensorflow_framework -O2
 
 if [ "$?" -ne 0 ]; then echo "compile rasterizer failed"; exit 1; fi
